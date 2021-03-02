scale normalization bug fixed

lib/core/inference.py
@@ -87,7 +87,6 @@ class PoseReconstructor(nn.Module):
     def __init__(self):
         super(PoseReconstructor, self).__init__()
         self.soft_argmax = SoftArgmax2D(window_fn='Parzen')
-        self.pdist = nn.PairwiseDistance(p=2, keepdim=True)
         self.p_key = 0
         self.c_key = 9
 
@@ -95,9 +94,8 @@ class PoseReconstructor(nn.Module):
         kps_25d = self.get_kps_25d(hm, dm)
         s = self.get_scaling_factor(kps_25d)
         kps_25d_hat = self.get_scale_normalized_pose(kps_25d, s)
-        return kps_25d_hat
-        #kps_3d_hat = self.reconstruct_3d_kps(kps_25d_hat, cam)
-        #return kps_3d_hat
+        kps_3d_hat = self.reconstruct_3d_kps(kps_25d_hat, cam)
+        return kps_3d_hat
 
     def get_kps_25d(self, batch_heatmap, batch_depthmap):
         for h, d in zip(batch_heatmap.shape, batch_depthmap.shape):
@@ -118,7 +116,7 @@ class PoseReconstructor(nn.Module):
         Returns:
             Batch Scaling Factor s (Shape = [Batch, 1])
         """
-        return self.pdist(kps_25d[:, self.p_key, :], kps_25d[:, self.c_key, :])
+        return (((kps_25d[:, self.p_key, :] - kps_25d[:, self.c_key, :]) ** 2).sum(1) ** 0.5).view(-1, 1)
 
     def get_scale_normalized_pose(self, kps_25d, s):
         """
@@ -165,13 +163,13 @@ class PoseReconstructor(nn.Module):
         Returns:
             Batch 3D KeyPoints (Shape = [Batch, Joint, 3])
         """
-        z_root = self.get_z_root(_kps_25d_hat)
         kps_25d_hat = _kps_25d_hat.clone()
-        K_inv = intrinsic_k.inverse()
-        K_inv = K_inv.repeat(20, 1, 1)
-        kps_25d_hat[:, :, 2] = 1
-        kps_3d_hat = (z_root + kps_25d_hat[:, :, 2]).view(-1, 1, 1) * K_inv.bmm(kps_25d_hat.view(-1, 3, 1))
-
+        z_root = self.get_z_root(kps_25d_hat).repeat(1, 20)
+        z_relative = kps_25d_hat[:, :, 2].clone()
+        kps_25d_hat[:, :, 2] = 1.0
+        #K_inv = intrinsic_k.inverse().unsqueeze(1).repeat(1, 20, 1, 1)
+        K = intrinsic_k.unsqueeze(1).repeat(1, 20, 1, 1)
+        kps_3d_hat = (z_relative + z_root).view(-1, 1, 1) * K.view(-1, 3, 3).bmm(kps_25d_hat.view(-1, 3, 1))
         return kps_3d_hat.view(-1, 20, 3)
 
     def procrustes_transform(self, kps_3d_hat, gt_kps_3d_hat):
@@ -184,5 +182,4 @@ class PoseReconstructor(nn.Module):
         Returns:
             Batch 3D KeyPoints Aligned to GT  (Shape = [Batch, Joint, 3])
         """
-        trans_kps_3d_hat = batch_compute_similarity_transform_torch(kps_3d_hat, gt_kps_3d_hat)
-        return trans_kps_3d_hat
+        return batch_compute_similarity_transform_torch(kps_3d_hat, gt_kps_3d_hat)

lib/core/loss.py
@@ -128,14 +128,18 @@ class LimbLengthLoss(nn.Module):
         self.human36_edge = [(0, 7), (7, 9), (9, 11), (11, 12), (9, 14), (14, 15), (15, 16), (9, 17), (17, 18),
                              (18, 19), (0, 1), (1, 2), (2, 3), (0, 4), (4, 5), (5, 6)]
         self.criterion = nn.MSELoss(reduction='mean')
+        self.p_key = 0
+        self.c_key = 9
 
     def get_limb_length(self, kps_3d):
-        human36_edge = [(0, 7), (7, 9), (9, 11), (11, 12), (9, 14), (14, 15), (15, 16), (9, 17), (17, 18),
-                        (18, 19), (0, 1), (1, 2), (2, 3), (0, 4), (4, 5), (5, 6)]
-        parent = kps_3d[:, [item[0] for item in human36_edge], :]
-        child = kps_3d[:, [item[1] for item in human36_edge], :]
+        s = self.get_scaling_factor(kps_3d)
+        parent = kps_3d[:, [item[0] for item in self.human36_edge], :]
+        child = kps_3d[:, [item[1] for item in self.human36_edge], :]
         dist = (((parent - child) ** 2).sum(2) ** 0.5)
-        return dist
+        return dist / s
+
+    def get_scaling_factor(self, kps_3d):
+        return (((kps_3d[:, self.p_key, :] - kps_3d[:, self.c_key, :]) ** 2).sum(1) ** 0.5).view(-1, 1)
 
     '''
         def get_limb_weight(self, output_weight):
@@ -226,10 +230,9 @@ class WeaklySupervisedLoss(nn.Module):
         self.criterion1 = JointsMSELoss(use_target_weight)
         self.criterion2 = LimbLengthLoss()
         self.criterion3 = MultiViewConsistencyLoss()
-        self.mse = nn.MSELoss(reduction='mean')
         self.use_target_weight = use_target_weight
-        self.alpha = 0.03
-        self.beta = 0.3
+        self.alpha = 1.0
+        self.beta = 10.0
         self.pose_reconstructor = PoseReconstructor()
 
     def forward(self, hm_outputs, dm_outputs, targets, target_weights, cameras, limb):